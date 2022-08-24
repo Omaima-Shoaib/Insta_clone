f79d85f23b8316719e0b021850544a481f701ebd
YomnaSherif19 <102757176+YomnaSherif19@users.noreply.github.com>
2022-08-24 20:45:27 +0200

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,6 +23,9 @@ class User extends Authenticatable implements MustVerifyEmail
         'phone',
         'email',
         'password',
+        'image',
+        'bio',
+        'website',
     ];
 
     /**
