1a6616e9bdbc2ee455fa1bc739bcbb234367fe3f
YomnaSherif19 <102757176+YomnaSherif19@users.noreply.github.com>
2022-08-25 00:43:17 +0200

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -40,29 +40,23 @@ class DatabaseSeeder extends Seeder
              'image'=>'avatar2.png',
 
          ]);
-        //following
-         \App\Models\followship::create([
-             'user1_id'=>$user2->id,//i am the logged in so user2 is following me
-             'user2_id'=>1,
-         ]);
-           //followers
-         \App\Models\followship::create([
-            'user1_id'=>1,//i am the logged in so user3 is following me
-             'user2_id'=>$user2->id,//this user2_id is in the followers of u1 
-         ]);
+        
+        
+//         //following
+//          \App\Models\followship::create([
+//              'user1_id'=>$user2->id,//i am the logged in so user2 is following me
+//              'user2_id'=>1,
+//          ]);
+//            //followers
+//          \App\Models\followship::create([
+//             'user1_id'=>1,//i am the logged in so user3 is following me
+//              'user2_id'=>$user2->id,//this user2_id is in the followers of u1 
+//          ]);
 
         
         
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
+  
         
         
     }
