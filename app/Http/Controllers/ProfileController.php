27e9395ff8843f51be01b07c6e50e836828d6c1a
YomnaSherif19 <102757176+YomnaSherif19@users.noreply.github.com>
2022-08-24 21:45:19 +0200

    Update ProfileController.php

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth as FacadesAuth;
 use Illuminate\Support\Facades\Auth;
 
-use Image;
+//use Image;
 
 class ProfileController extends Controller{
 
