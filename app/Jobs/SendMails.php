b9044734d75555a0c91ed8d7090b91848b50c08f
AyaIbrahim3027 <102617431+AyaIbrahim3027@users.noreply.github.com>
2022-08-26 01:21:21 +0200

    Update SendMails.php

--- app/Jobs/SendMails.php
+++ app/Jobs/SendMails.php
@@ -15,26 +15,26 @@ class SendMails implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    /**
-     * Create a new job instance.
-     *
-     * @return void
-     */
-    public $data;
-    public function __construct($data)
-    {
-        $this -> data = $data;
-    }
+//     /**
+//      * Create a new job instance.
+//      *
+//      * @return void
+//      */
+//     public $data;
+//     public function __construct($data)
+//     {
+//         $this -> data = $data;
+//     }
 
-    /**
-     * Execute the job.
-     *
-     * @return void
-     */
-    public function handle()
-    {
-        foreach ($this -> data as $_data){
-           Mail::to ($_data -> email) -> send (new \App\Mail\TestMail());
-        }
-    }
+//     /**
+//      * Execute the job.
+//      *
+//      * @return void
+//      */
+//     public function handle()
+//     {
+//         foreach ($this -> data as $_data){
+//            Mail::to ($_data -> email) -> send (new \App\Mail\TestMail());
+//         }
+//     }
 }
