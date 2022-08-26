a7dcc5e34d04ffe7732d0ce16040913e7f7ab123
YomnaSherif19 <102757176+YomnaSherif19@users.noreply.github.com>
2022-08-26 23:54:44 +0200

    Update peopleoninstagram.blade.php

--- resources/views/peopleoninstagram/peopleoninstagram.blade.php
+++ resources/views/peopleoninstagram/peopleoninstagram.blade.php
@@ -1,29 +1,14 @@
-<nav style="display:flex-box;float:right;">
-<a href="{{ route('peopleoninstagram.peopleoninstagram') }}" >People on Instagram</a>
-<a href="{{ route('users.index') }}" >My profile</a>
-<form method="POST" action="{{ route('logout') }}">
-                            @csrf
+@extends('layouts.navbar')
+@section('content')
 
-                            <a href="route('logout')"
-                                    onclick="event.preventDefault();
-                                                this.closest('form').submit();">
-                                {{ __('Log Out') }}
-                            </a>
-                        </form>
-
-</nav>
 <h2>Hi {{Auth::user()->name}}, Find people on instagram </h2>
-
-
-
-
 
 @foreach($user as $item3)
 <div class="follwinglist" style="display:flex ;">
-<img class="image rounded-circle" src="{{asset('/avatars/'.$item3->image)}}" alt="profile_image" style="width: 80px;height: 80px; padding: 10px; margin: 0px;border-radius: 50%; ">
+<img class="image rounded-circle" src="{{asset('/avatars/'.$item3->image)}}" alt="profile_image" style="width: 90px;height: 90px; padding: 10px; margin: 0px;border-radius: 50%; ">
 
-<div class="followship-username float-left" style="display:flexbox; ;">
-   <a href="#" style="text-decoration: none ;"><h3> {{$item3->name}}</h3></a>
+<div class="followship-username float-left" style="display:flexbox; margin-left:30px ; ">
+   <a href="#" style="text-decoration: none ;  font-size:20px;"> <h3> {{$item3->name}}</h3> </a>
 
    @if(isFollowing($item3->id)=='following')
     <button class="float-right following followingship-status">Following </button>
@@ -42,7 +27,11 @@
 </div>
     </div>
     
+<hr>
 
 @endforeach
+
+@endsection
+
 
 
