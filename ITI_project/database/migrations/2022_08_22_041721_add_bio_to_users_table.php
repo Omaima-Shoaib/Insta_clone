8691197f25691f0b06a9f89ad8401a034ee9c622
YomnaSherif19 <102757176+YomnaSherif19@users.noreply.github.com>
2022-08-27 00:06:34 +0200

    yomna

--- ITI_project/database/migrations/2022_08_22_041721_add_bio_to_users_table.php
+++ ITI_project/database/migrations/2022_08_22_041721_add_bio_to_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('bio')->default('');
+            $table->string('bio')->default('')->nullable();
         });
     }
 
