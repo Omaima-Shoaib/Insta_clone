78c0b20ad31d0cbd16ce8a5bba7253ce2523dce8
YomnaSherif19 <102757176+YomnaSherif19@users.noreply.github.com>
2022-08-27 00:23:44 +0200

    yomna

--- ITI_project/resources/views/peopleoninstagram/peopleoninstagram.blade.php
+++ ITI_project/resources/views/peopleoninstagram/peopleoninstagram.blade.php
@@ -1,29 +1,16 @@
-<nav style="display:flex-box;float:right;">
-<a href="{{ route('peopleoninstagram.peopleoninstagram') }}" >People on Instagram</a>
-<a href="{{ route('users.index') }}" >My profile</a>
-<form method="POST" action="{{ route('logout') }}">
-                            @csrf
-
-                            <a href="route('logout')"
-                                    onclick="event.preventDefault();
-                                                this.closest('form').submit();">
-                                {{ __('Log Out') }}
-                            </a>
-                        </form>
-
-</nav>
+@extends('layouts.navbar')
+@section('content')
 <h2>Hi {{Auth::user()->name}}, Find people on instagram </h2>
-
-
-
-
 
 @foreach($user as $item3)
 <div class="follwinglist" style="display:flex ;">
-<img class="image rounded-circle" src="{{asset('/storage/images/'.$item3->image)}}" alt="profile_image" style="width: 80px;height: 80px; padding: 10px; margin: 0px;border-radius: 50%; ">
+<img class="image rounded-circle" src="{{asset('/avatars/'.$item3->image)}}" alt="profile_image" style="width: 90px;height: 90px; padding: 10px; margin: 0px;border-radius: 50%; ">
 
-<div class="followship-username float-left" style="display:flexbox; ;">
-   <a href="#" style="text-decoration: none ;"><h3> {{$item3->name}}</h3></a>
+
+
+
+<div class="followship-username float-left" style="display:flexbox;margin-left:30px ;">
+   <a href="#" style="text-decoration: none ; font-size:20px;"><h3> {{$item3->name}}</h3></a>
 
    @if(isFollowing($item3->id)=='following')
     <button class="float-right following followingship-status">Following </button>
@@ -39,10 +26,17 @@
     <button class="float-right followed followingship-status">Follow </button>
 
      @endif
+
+
 </div>
     </div>
     
+    <hr>
 
 @endforeach
+
+@endsection
+
+
 
 
