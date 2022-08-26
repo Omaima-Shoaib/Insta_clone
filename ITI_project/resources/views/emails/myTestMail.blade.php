0a4e42fd2b0abff92795f95dd13bd8df499dfc8b
AyaIbrahim3027 <102617431+AyaIbrahim3027@users.noreply.github.com>
2022-08-27 00:24:35 +0200

    aya

---
+++ ITI_project/resources/views/emails/myTestMail.blade.php
@@ -0,0 +1,17 @@
+<!-- <h2>
+    HI , {{ $name }}
+</h2>
+<br> -->
+<h2>
+    New user has registered!
+</h2>
+<br>
+<!-- <h2>
+     Email: {{ $email }}
+</h2> -->
+<br>
+<h2>
+    Thanks for using our application!
+</h2>
+
+
